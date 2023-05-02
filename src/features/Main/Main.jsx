5ce807a62807d6c8f731abdb2c18978f74e6e1af
bbenoi01 <b.nutty@gmail.com>
2023-05-01 19:36:20 -0700

    added up next and rsvp components

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
+import UpNext from './components/UpNext';
+import RSVP from './components/RSVP';
 
 const Main = () => {
 	return (
 		<div className='canvas'>
 			<Hero />
+			<UpNext />
+			<RSVP />
 		</div>
 	);
 };
--- src/features/Main/components/Hero/Hero.jsx
+++ src/features/Main/components/Hero/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './hero.scss';
 
 const Hero = () => {
-	return <div className='hero'>Let's Do Brunch!</div>;
+	return (
+		<div className='hero'>
+			<video src='brunch.mp4' muted autoPlay loop />
+			<div className='hero-txt'>
+				<h1>Let's Do Brunch!</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
