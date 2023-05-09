597f31b981b7ef54252ffa083bfc10dfaf47e017
bbenoi01 <b.nutty@gmail.com>
2023-05-09 08:49:57 -0700

    added welcome section

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Welcome from './components/Welcome';
 import UpNext from './components/UpNext';
 import RSVP from './components/RSVP';
 
@@ -7,6 +8,7 @@ const Main = () => {
 	return (
 		<div className='canvas'>
 			<Hero />
+			<Welcome />
 			<UpNext />
 			<RSVP />
 		</div>
