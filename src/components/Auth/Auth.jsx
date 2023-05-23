b082074da00665f8629d6c8515922a569bb5a5f2
bbenoi01 <b.nutty@gmail.com>
2023-05-23 16:03:53 -0700

    added notify to register

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,4 +1,12 @@
-import { FormControl, IconButton, InputAdornment } from '@mui/material';
+import {
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	IconButton,
+	InputAdornment,
+	Radio,
+	RadioGroup,
+} from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import {
 	register,
@@ -9,6 +17,7 @@ import {
 	setPhone,
 	setEmail,
 	setPassword,
+	setNotify,
 	setShow,
 	clearErrors,
 	clearForm,
@@ -31,6 +40,7 @@ const Auth = () => {
 		phone,
 		email,
 		password,
+		notify,
 		show,
 		errors,
 	} = useSelector((state) => state.auth);
@@ -64,6 +74,10 @@ const Auth = () => {
 				dispatch(setPassword(value));
 				break;
 
+			case 'notify':
+				dispatch(setNotify(value));
+				break;
+
 			default:
 				break;
 		}
@@ -86,6 +100,7 @@ const Auth = () => {
 				data.firstName = firstName;
 				data.lastName = lastName;
 				data.phone = phone;
+				data.notify = notify;
 				dispatch(register(data));
 				break;
 
@@ -205,6 +220,48 @@ const Auth = () => {
 					/>
 					{errors && errors.password && (
 						<h6 className='error'>{errors.password}</h6>
+					)}
+					{authType === 'Register' && (
+						<div className='notify'>
+							<FormLabel className='notify-label'>
+								How do want to be notified?
+							</FormLabel>
+							<RadioGroup
+								row
+								value={notify}
+								onChange={(e) => handleChange('notify', e.target.value)}
+							>
+								<FormControlLabel
+									className='notify-label'
+									value='sms'
+									control={
+										<Radio
+											sx={{
+												color: 'whitesmoke',
+												'&.Mui-checked': { color: 'rgb(156, 39, 176)' },
+											}}
+										/>
+									}
+									label='Text'
+								/>
+								<FormControlLabel
+									className='notify-label'
+									value='email'
+									control={
+										<Radio
+											sx={{
+												color: 'whitesmoke',
+												'&.Mui-checked': { color: 'rgb(156, 39, 176)' },
+											}}
+										/>
+									}
+									label='Email'
+								/>
+							</RadioGroup>
+						</div>
+					)}
+					{errors && errors.notify && (
+						<h6 className='error'>{errors.notify}</h6>
 					)}
 					<Button type='submit' loading={loading} label='SUBMIT' />
 				</FormControl>
